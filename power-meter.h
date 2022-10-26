125e54a22ace0faef5775010bdbee67540db7c24
Thorsten Lanfer <tlanfer@gmail.com>
2022-10-26 22:05:10 +0200

    fix the power meter

--- power-meter.h
+++ power-meter.h
@@ -1,18 +1,18 @@
 #include "esphome.h"
 
-#define POLLING_INTERVAL 30
-#define AVG_OVER 10
-#define DEBOUNCE_DURATION 150
-#define ONE_HOUR 3600000
+#define POLLING_INTERVAL 50
+#define DEBOUNCE_DURATION 250
 
+#define AVG_OVER 10
 #define BLINK_TIME 1
+
+#define ONE_HOUR 3600000
 
 #define LED_PIN D1
 #define LED_PIN_GND D2
-
 #define SENSOR_PIN A0
 
-class MyPowerSensor : public Component, public Sensor {
+class MyPowerSensor : public Component {
 
     private:
         unsigned long lastRead;
@@ -20,7 +20,10 @@ class MyPowerSensor : public Component, public Sensor {
 
         unsigned long risingEdgeTimeout;
         long threshold = 0;
+
         bool wasHigh = false;
+
+        double total = 0;
 
         long measureValue() {
             long val = 0;
@@ -45,6 +48,9 @@ class MyPowerSensor : public Component, public Sensor {
         }
 
     public:
+        Sensor *power_sensor = new Sensor();
+        Sensor *total_sensor = new Sensor();
+
         MyPowerSensor(long _threshold) {
             threshold = _threshold;
         }
@@ -64,7 +70,7 @@ class MyPowerSensor : public Component, public Sensor {
             if( now - lastRead > POLLING_INTERVAL) {
                 lastRead = now;
                 long val = measureValue();
-//                ESP_LOGD("custom", "Measured: %d", val);
+//                ESP_LOGI("custom", "Measured: %d", val);
                 bool isHigh = val > threshold;
 
                 if( val > 200) {
@@ -73,30 +79,43 @@ class MyPowerSensor : public Component, public Sensor {
 
                 // rising edge
                 if( isHigh && !wasHigh){
-                    ESP_LOGD("custom", "Rising edge");
+//                    ESP_LOGI("custom", "Rising edge: %d", val);
                     risingEdgeTimeout = now + DEBOUNCE_DURATION;
                 }
 
                 // falling edge
                 if( !isHigh && wasHigh) {
-                    ESP_LOGD("custom", "Falling edge");
+//                    ESP_LOGI("custom":, "Falling edge %d", val);
+
+                    if( risingEdgeTimeout > 0 && now > risingEdgeTimeout) {
+                        if( lastPublishTime > 0) {
+                            unsigned long timeBetween = now - lastPublishTime;
+                            float result = ((1.0*ONE_HOUR)/timeBetween)/75.0;
+
+//                            ESP_LOGI("custom", "Publish %f kW over %d ms", result, timeBetween);
+                            total += 1.0/75.0;
+                            power_sensor->publish_state(result);
+                            total_sensor->publish_state(total);
+                        }
+
+                        lastPublishTime = now;
+                    }
+
                     risingEdgeTimeout = 0;
                 }
 
                 // check if high for long enough
-                if( risingEdgeTimeout > 0 && risingEdgeTimeout > now) {
-                    if( lastPublishTime > 0) {
-                        unsigned long timeBetween = now - lastPublishTime;
-                        float result = ((1.0*ONE_HOUR)/timeBetween)/75.0;
-
-                        ESP_LOGI("custom", "Publish %f kW over %d ms", result, timeBetween);
-
-                        publish_state(result);
-                    }
-
-                    risingEdgeTimeout = 0;
-                    lastPublishTime = now;
-                }
+//                if( isHigh && risingEdgeTimeout > 0 && risingEdgeTimeout > now) {
+//                    if( lastPublishTime > 0) {
+//                        unsigned long timeBetween = now - lastPublishTime;
+//                        float result = ((1.0*ONE_HOUR)/timeBetween)/75.0;
+//
+////                        ESP_LOGI("custom", "Publish %f kW over %d ms", result, timeBetween);
+////                        publish_state(result);
+//                    }
+//
+//                    risingEdgeTimeout = 0;
+//                }
 
                 wasHigh = isHigh;
             }
